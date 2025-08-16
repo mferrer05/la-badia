4349d673f364b6105a44f218c16d1bc3b6fee0c6
Marc Ferrer <marcferrer945@gmail.com>
2025-08-16 07:55:14 +0200

    Add Rollup plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { visualizer } from "rollup-plugin-visualizer";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      visualizer({
+        emitFile: true,
+        filename: "stats.html",
+      }),
+    ],
   },
 });
